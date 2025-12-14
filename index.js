bd8f2fc182f99026c48cfacd3fc2ca5f4ff48e0b
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-12-14 14:23:55 +0600

    new code added

--- index.js
+++ index.js
@@ -13,12 +13,15 @@ const circle = createCircle(1);
 
 //constructor function
 function Circle(radius) {
-  console.log(`this:`, this);
+  //   console.log(`this:`, this);
   this.radius = radius;
   this.draw = function () {
     console.log("This is drawn.");
   };
 }
+
+Circle.call({}, 1);
+Circle.apply({}, [1, 2, 3]);
 
 const anotherCircle = new Circle(1);
 // anotherCircle.draw();

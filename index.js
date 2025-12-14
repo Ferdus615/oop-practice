a3de56f163560d09e3ae9f97bc0ab4bd55d524cb
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-12-14 14:06:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// Factory function
+function createCircle(radius) {
+  return {
+    radius,
+
+    draw: function () {
+      console.log("This is drawn.");
+    },
+  };
+}
+
+const circle = createCircle(1);
+
+//constructor function
+function Circle(radius) {
+  console.log(`this:`, this);
+  this.radius = radius;
+  this.draw = function () {
+    console.log("This is drawn.");
+  };
+}
+
+const anotherCircle = new Circle(1);
+// anotherCircle.draw();
+
+// const Circle1 = new Function(
+//   "radius",
+//   `
+//   this.radius = radius;
+//   this.draw = function () {
+//     console.log("This is drawn.");
+//   };
+// `
+// );
+// const circle2 = new Circle1(1);
+// circle2.draw();
